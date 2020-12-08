4d79fa3aa26f6ab25019e2d43554abba32cd6784
Zakk Raines <zakkary8195@gmail.com>
2020-12-08 19:21:19 +0000

    manipulating lists

---
+++ listmethods/listmeth03.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+movies = ["300", "Scarface", "Spiderman"]
+print(movies)
+new_movies = ["The Godfather", "Tarzan"]
+print(new_movies)
+movies.insert(new_movies[2])
+print(movies)
