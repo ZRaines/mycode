931023cf0f5fa4e2942586db137d27e8e84672f7
Zakk Raines <zakkary8195@gmail.com>
2020-12-16 16:08:43 +0000

    hitman proj

---
+++ hitman.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+""" Hitman Project | Author: Zakkary Raines """
+
+import time
+import crayons
+
+round = 0
+print("Welcome back Agent 47.")
+time.sleep(1)
+print("I hope your last mission was a success.")
+time.sleep(2)
+input("Are you ready for another contract?")
+if input is "yes" or "Yes":
+    print("Alright then, log into the database and see what's available.")
+    #database = input("Username:")
+elif input is "no" or "No":
+    print("Perhaps another time then.")
+break
+else:
+    print("Sorry I didn't catch that. There may be interference on your end.")
+    
+#database = input("Username:")
+#if database == "Agent 47":
+ #   input("Password:")
+#elif database != "Agent 47":
+   # print("Username not recognized. Please insert a valid Username.")
+    #print(crayons.red('ACCESS DENIED!'))
+
+#if database == "Agent 47":
+    #print(crayons.green('Access granted', time.now()))
+
