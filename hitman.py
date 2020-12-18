a58e7392541a69059f895e6c8e286b25c1a9e0f9
Zakk Raines <zakkary8195@gmail.com>
2020-12-18 15:37:16 +0000

    tweaking proj

--- hitman.py
+++ hitman.py
@@ -4,33 +4,51 @@
 
 import time
 import crayons
+from sys import exit as ex
 
+
+mission = ""
+database = ""
+passwd = ""
 round = 0
+
 print("Welcome back Agent 47.")
 time.sleep(1)
 print("I hope your last mission was a success.")
 time.sleep(2)
-mission = input("Are you ready for another contract?")
-while True:
+
+while round < 3 or mission.lower != "yes" or "no":
+    round += 1
+    mission = input("Are you ready for another contract?")
     if mission.lower() == "yes":
-        print("Alright then, log into the database and see what's available.")
-        database = input("Username:")
-        if database == "Agent 47":
-            passwd = input("Password:")
-            if passwd == "ghost":
-               print(crayons.green('Access Granted', time)    
-        elif database != "Agent 47":
-        error = input("Username not recognized. Please insert a valid Username.")
-        #passwd = input("Password:")
-            elif mission.lower() == "no":
-            print("Perhaps another time then.")
-            break
+        print("Alright then, log into the database and see who's available.")
+        break
+    elif round == 3:
+        print("Poor connection, disconnecting for security purposes.")
+        ex()
+    elif mission.lower() == "no":
+        print("Perhaps another time then.")
+        ex()
     else:
-        print("Sorry I didn't catch that. There may be interference on your end.")        
+        print("Sorry I didn't catch that.")        
 
-    
-    #print(crayons.red('ACCESS DENIED!'))
+while round < 5 and database != "Agent 47":
+    round += 1
+    database = input("Username:")
+    if database == "Agent 47":
+        print("Authorized User, enter your password.")
+    elif round == 5:
+        print("Could not validate user, ending transmission")
+        ex()
+    else:
+        print("Invalid Username. Insert a valid Username.")
 
-#if database == "Agent 47":
-    #print(crayons.green('Access granted', time.now()))
-
+while round < 6 and passwd != "ghost":
+    round += 1
+    passwd = input("Password:")
+    if passwd == "ghost":
+        print(crayons.green('Access Granted'))
+    elif round == 6:
+        print(crayons.red('ACCESS DENIED!!! Agent may be compromised. Goodbye.'))
+    else:
+        print("Incorrect password. Try again.")
