d43a421e19ca6d81a5f7bb927d9b59f571629afc
Zakk Raines <zakkary8195@gmail.com>
2020-12-18 00:00:16 +0000

    project

--- hitman.py
+++ hitman.py
@@ -10,21 +10,25 @@ print("Welcome back Agent 47.")
 time.sleep(1)
 print("I hope your last mission was a success.")
 time.sleep(2)
-input("Are you ready for another contract?")
-if input is "yes" or "Yes":
-    print("Alright then, log into the database and see what's available.")
-    #database = input("Username:")
-elif input is "no" or "No":
-    print("Perhaps another time then.")
-break
-else:
-    print("Sorry I didn't catch that. There may be interference on your end.")
+mission = input("Are you ready for another contract?")
+while True:
+    if mission.lower() == "yes":
+        print("Alright then, log into the database and see what's available.")
+        database = input("Username:")
+        if database == "Agent 47":
+            passwd = input("Password:")
+            if passwd == "ghost":
+               print(crayons.green('Access Granted', time)    
+        elif database != "Agent 47":
+        error = input("Username not recognized. Please insert a valid Username.")
+        #passwd = input("Password:")
+            elif mission.lower() == "no":
+            print("Perhaps another time then.")
+            break
+    else:
+        print("Sorry I didn't catch that. There may be interference on your end.")        
+
     
-#database = input("Username:")
-#if database == "Agent 47":
- #   input("Password:")
-#elif database != "Agent 47":
-   # print("Username not recognized. Please insert a valid Username.")
     #print(crayons.red('ACCESS DENIED!'))
 
 #if database == "Agent 47":
